ad41923b0c3dbdb9d3fa6ec50147b26bba551157
AntonJanto <anton.janto@gmail.com>
2021-05-12 13:42:35 +0200

    Car class update, Created classes MailBox and Person

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,31 @@
 public class Car
 {
+     private String model;
+     private String make;
+
+     public Car(String model, String make)
+     {
+          this.model = model;
+          this.make = make;
+     }
+
+     public String getModel()
+     {
+          return model;
+     }
+
+     public void setModel(String model)
+     {
+          this.model = model;
+     }
+
+     public String getMake()
+     {
+          return make;
+     }
+
+     public void setMake(String make)
+     {
+          this.make = make;
+     }
 }
