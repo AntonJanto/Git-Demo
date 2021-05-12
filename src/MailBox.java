b175460bbbf06597b0bb618f00e46b6b80c81704
AntonJanto <anton.janto@gmail.com>
2021-05-12 14:19:50 +0200

    MailBox has a Person

--- src/MailBox.java
+++ src/MailBox.java
@@ -1,3 +1,19 @@
 public class MailBox
 {
+     private Person person;
+
+     public MailBox(Person person)
+     {
+          this.person = person;
+     }
+
+     public Person getPerson()
+     {
+          return person;
+     }
+
+     public void setPerson(Person person)
+     {
+          this.person = person;
+     }
 }
