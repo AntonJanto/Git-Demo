178dea6c5f2a56a83368cb8de3c4f4e4d78e2efc
AntonJanto <anton.janto@gmail.com>
2021-05-12 14:24:48 +0200

    Truck is a car

--- src/Truck.java
+++ src/Truck.java
@@ -1,3 +1,9 @@
-public class Truck
+public class Truck extends Car
 {
+     private double weight;
+
+     public Truck(String model, String make, String licenseNumber, int year)
+     {
+          super(model, make, licenseNumber, year);
+     }
 }
