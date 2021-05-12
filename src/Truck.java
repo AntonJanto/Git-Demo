a28bd2bf5eaec6b748dd6cdc0e3a4a17246fb106
AntonJanto <anton.janto@gmail.com>
2021-05-12 14:35:10 +0200

    I made changes on my branch

--- src/Truck.java
+++
@@ -1,9 +0,0 @@
-public class Truck extends Car
-{
-     private double weight;
-
-     public Truck(String model, String make, String licenseNumber, int year)
-     {
-          super(model, make, licenseNumber, year);
-     }
-}
