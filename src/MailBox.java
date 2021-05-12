8c9203db4772f30656878f2ec8aeb593b464afd2
AntonJanto <anton.janto@gmail.com>
2021-05-12 14:28:27 +0200

    MailBox has a name

--- src/MailBox.java
+++ src/MailBox.java
@@ -2,11 +2,13 @@ public class MailBox
 {
      private Person person;
      private int age;
+     private String name;
 
-     public MailBox(Person person, int age)
+     public MailBox(Person person, int age, String name)
      {
           this.person = person;
           this.age= age;
+          this.name = name;
      }
 
      public Person getPerson()
